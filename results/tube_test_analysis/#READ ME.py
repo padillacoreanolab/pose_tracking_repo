1602a25661b65b118bf8430c8c781c266a4b83c2
mcum96 <meghanic96@gmail.com>
2023-12-08 10:18:52 -0500

    Fnctions for feature extraction, UMAP, cluster gifs, and feature visualization for RC

---
+++ results/tube_test_analysis/#READ ME.py
@@ -0,0 +1,15 @@
+#READ ME 
+
+conda install -c anaconda h5py -y
+conda install numpy -y
+conda install scipy -y
+conda install -c conda-forge matplotlib -y
+conda install -c conda-forge scikit-learn -y
+conda install -c conda-forge umap-learn -y
+conda install -c conda-forge hdbscan -y
+conda install -c anaconda seaborn -y
+conda install -c anaconda pandas -y
+conda install -c conda-forge pickle5 -y
+conda install -c conda-forge opencv -y
+conda install -c conda-forge moviepy -y
+conda install -c conda-forge imageio -y
